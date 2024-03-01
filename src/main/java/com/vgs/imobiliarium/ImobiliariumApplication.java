a8afd55c461ad59d64ffc027697d14917dfa692b
Vinicius Gabriel Saggioratto <viniciussaggioratto@gmail.com>
2024-03-01 20:43:09 -0300

    feat/base do sistema.

---
+++ src/main/java/com/vgs/imobiliarium/ImobiliariumApplication.java
@@ -0,0 +1,13 @@
+package com.vgs.imobiliarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobiliariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobiliariumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vgs/imobiliarium/ImobiliariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vgs.imobiliarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
